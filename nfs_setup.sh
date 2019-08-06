3314bacb76de6bebd7a6e77e3fbc61e55446d452
Mykhailo <molochko@adobe.com>
2019-08-06 15:38:56 +0300

    nfs setup script

---
+++ nfs_setup.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+U=`id -u`
+G=`id -g`
+
+echo ""
+echo " +-----------------------------+"
+echo " | Setup native NFS for Docker |"
+echo " +-----------------------------+"
+echo ""
+
+echo "WARNING: This script will shut down running containers."
+echo ""
+echo -n "Do you wish to proceed? [y]: "
+read decision
+
+if [ "$decision" != "y" ]; then
+echo "Exiting. No changes made."
+exit 1
+fi
+
+echo ""
+
+if ! docker ps > /dev/null 2>&1 ; then
+echo "== Waiting for docker to start..."
+fi
+
+open -a Docker
+
+while ! docker ps > /dev/null 2>&1 ; do sleep 2; done
+
+echo "== Stopping running docker containers..."
+docker-compose down > /dev/null 2>&1
+docker volume prune -f > /dev/null
+
+osascript -e 'quit app "Docker"'
+
+echo "== Resetting folder permissions..."
+
+sudo chown -R "$U":"$G" .
+
+echo "== Setting up nfs..."
+LINE="/Users -alldirs -mapall=$U:$G localhost"
+FILE=/etc/exports
+sudo cp /dev/null $FILE
+grep -qF -- "$LINE" "$FILE" || sudo echo "$LINE" | sudo tee -a $FILE > /dev/null
+
+LINE="nfs.server.mount.require_resv_port = 0"
+FILE=/etc/nfs.conf
+grep -qF -- "$LINE" "$FILE" || sudo echo "$LINE" | sudo tee -a $FILE > /dev/null
+
+echo "== Restarting nfsd..."
+sudo nfsd restart
+
+echo "== Restarting docker..."
+open -a Docker
+
+while ! docker ps > /dev/null 2>&1 ; do sleep 2; done
