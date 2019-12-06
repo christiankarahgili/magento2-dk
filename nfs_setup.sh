82c25071f172e092eb78cd58eca42ad97e7adff0
Mykhailo <molochko@adobe.com>
2019-12-06 16:38:32 +0000

    ready for frontend developing

--- nfs_setup.sh
+++ nfs_setup.sh
@@ -9,7 +9,6 @@ echo " | Setup native NFS for Docker |"
 echo " +-----------------------------+"
 echo ""
 
-#echo "WARNING: This script will shut down running containers."
 echo ""
 echo -n "Do you wish to proceed? [y]: "
 read decision
@@ -20,20 +19,6 @@ exit 1
 fi
 
 echo ""
-
-#if ! docker ps > /dev/null 2>&1 ; then
-#echo "== Waiting for docker to start..."
-#fi
-
-#open -a Docker
-
-#while ! docker ps > /dev/null 2>&1 ; do sleep 2; done
-
-#echo "== Stopping running docker containers..."
-#docker-compose down > /dev/null 2>&1
-##docker volume prune -f > /dev/null
-
-#osascript -e 'quit app "Docker"'
 
 echo "== Resetting folder permissions..."
 
@@ -51,8 +36,3 @@ grep -qF -- "$LINE" "$FILE" || sudo echo "$LINE" | sudo tee -a $FILE > /dev/null
 
 echo "== Restarting nfsd..."
 sudo nfsd restart
-
-#echo "== Restarting docker..."
-#open -a Docker
-
-#while ! docker ps > /dev/null 2>&1 ; do sleep 2; done
